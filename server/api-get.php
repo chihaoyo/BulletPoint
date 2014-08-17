509427e16e5b99d00f210b733127453625bea2a3
chihaoyo <chihaoyo@gmail.com>
2014-08-17 00:51:20 -0400

    Expand REST

---
+++ server/api-get.php
@@ -0,0 +1,14 @@
+<?php
+
+function get_all($table) {
+	$q = 'SELECT * FROM :table WHERE 1';
+	$p = array('table' => $table);
+	return $db->fa($q, $p);
+}
+function get($db, $table, $serial) {
+	$q = 'SELECT * FROM :table WHERE serial = :serial';
+	$p = array('table' => $table, 'serial' => $serial);
+	return $db->f1($q, $p);
+}
+
+?>
--- server/api.php
+++ server/api.php
@@ -1,33 +1,34 @@
 <?php
 
 include_once('importer.php');
+include_once('api-get.php');
 
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
-$app->get('/nodes/:id', function($id) use ($app) {
+$app->get('/:table/:id', function($table, $id) use ($app) {
 	if($id == 'all') {
-		___('get all Nodes');
+		___("get all $table")
 	}
 	else if(is_numeric($id) && intval($id) > 0) {
 		$id = intval($id);
-		___('get Node #' . $id);
+		___("get $table #$id";
 	}
 	else {
 		$app->notFound();
 	}
 
 });
-$app->post('/nodes', function() use ($app) {
-	___('post to Nodes');
+$app->post('/:table', function($table) use ($app) {
+	___("post to $table" . 's');
 	$parameters = $app->request->post();
 	___($parameters);
 });
-$app->delete('/nodes/:id', function($id) use ($app){
+$app->delete('/$table/:id', function($table, $id) use ($app){
 	if(is_numeric($id) && intval($id) > 0) {
 		$id = intval($id);
-		___('delete Node #' . $id);
+		___("delete $table #$id");
 	}
 	else {
 		$app->notFound();
