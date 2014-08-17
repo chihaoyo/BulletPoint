a89f649dce3c02d91b0ed2df96ce142b99601cc5
chihaoyo <chihaoyo@gmail.com>
2014-08-17 01:39:31 +0000

    REST API

---
+++ server/api.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once('importer.php');
+
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+$app->get('/nodes/:id', function($id) use ($app) {
+	if($id == 'all') {
+		___('get all Nodes');
+	}
+	else if(is_numeric($id) && intval($id) > 0) {
+		$id = intval($id);
+		___('get Node #' . $id);
+	}
+	else {
+		$app->notFound();
+	}
+
+});
+$app->post('/nodes', function() {
+	___('post to Nodes');
+});
+$app->delete('/nodes/:id', function($id) use ($app){
+	if(is_numeric($id) && intval($id) > 0) {
+		$id = intval($id);
+		___('delete Node #' . $id);
+	}
+	else {
+		$app->notFound();
+	}
+});
+$app->notFound(function() {
+	___('not available');
+});
+$app->run();
+
+?>
