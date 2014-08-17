f7b0555bab268ab2837b639314da2186a514ad41
chihaoyo <chihaoyo@gmail.com>
2014-08-17 05:20:06 +0000

    Implement REST GET

--- server/api-get.php
+++ server/api-get.php
@@ -1,13 +1,12 @@
 <?php
 
 function get_all($db, $table) {
-	$q = 'SELECT * FROM :table WHERE 1';
-	$p = array('table' => $table);
-	return $db->fa($q, $p);
+	$q = "SELECT * FROM $table WHERE 1";
+	return $db->fa($q);
 }
 function get($db, $table, $serial) {
-	$q = 'SELECT * FROM :table WHERE serial = :serial';
-	$p = array('table' => $table, 'serial' => $serial);
+	$q = "SELECT * FROM $table WHERE serial = :serial";
+	$p = array('serial' => $serial);
 	return $db->f1($q, $p);
 }
 
--- server/api.php
+++ server/api.php
@@ -1,31 +1,39 @@
 <?php
 
 include_once('importer.php');
-//include_once('api-get.php');
+include_once('api-get.php');
 
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
+$db = connect_to_db();
+
 $app = new \Slim\Slim();
-$app->get('/:table/:id', function($table, $id) use ($app) {
+$app->get('/:table/:id', function($table, $id) use ($app, $db) {
+	$table = ucfirst($table);
+	
 	if($id == 'all') {
-		___("get all $table")
+		echo json_encode(get_all($db, $table));
 	}
 	else if(is_numeric($id) && intval($id) > 0) {
 		$id = intval($id);
-		___("get $table #$id");
+		echo json_encode(get($db, $table, $id));
 	}
 	else {
 		$app->notFound();
 	}
 
 });
-$app->post('/:table', function($table) use ($app) {
-	___("post to $table" . 's');
+$app->post('/:table', function($table) use ($app, $db) {
+	$table = ucfirst($table);
+	
+	___("post to $table");
 	$parameters = $app->request->post();
 	___($parameters);
 });
-$app->delete('/$table/:id', function($table, $id) use ($app){
+$app->delete('/:table/:id', function($table, $id) use ($app, $db){
+	$table = ucfirst($table);
+	
 	if(is_numeric($id) && intval($id) > 0) {
 		$id = intval($id);
 		___("delete $table #$id");
