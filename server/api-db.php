c52320eb15affc417d70b2dfc686331e410e4b6d
chihaoyo <chihaoyo@gmail.com>
2014-08-17 16:54:55 -0400

    Update POST in REST

---
+++ server/api-db.php
@@ -0,0 +1,39 @@
+<?php
+
+$db_structure = array(
+	'Nodes' => array('user_id', 'url', 't_create', 't_update', 'title', 'comment'), 
+	'Edges', => array(), 
+	'Tags', => array('tag'), 
+	'NodeTagPairs' => array('node_id', 'tag_id')
+);
+
+function db_structure_all_tables() { return array_keys($db_structure); }
+
+function table_exists($table) {
+	return in_array($table, db_structure_all_tables());
+}
+function has_all_required_cols($table, $data) {
+	$required_cols = $db_structure[$table];
+	$present_cols = array_keys($data);
+	$keys_present_in_both = array_intersect($required_cols, $present_cols);
+	___($required_cols);
+	___($present_cols);
+	___($keys_present_in_both);
+}
+
+// $table cannot be in $p
+function select_all($db, $table) {
+	$q = "SELECT * FROM $table WHERE 1";
+	return $db->fa($q);
+}
+function select($db, $table, $serial) {
+	$q = "SELECT * FROM $table WHERE serial = :serial";
+	$p = array('serial' => $serial);
+	return $db->f1($q, $p);
+}
+function insert($db, $table, $data) {
+	if(!has_all_required_cols($table, $data))
+		return false;
+}
+
+?>
--- server/api.php
+++ server/api.php
@@ -1,11 +1,7 @@
 <?php
 
 include_once('importer.php');
-include_once('api-get.php');
-
-function table_exists($table) {
-	return in_array($table, array('Nodes', 'Edges', 'Tags', 'NodeTagPairs'));
-}
+include_once('api-db.php');
 
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
@@ -20,11 +16,11 @@ $app->get('/:table/:id', function($table, $id) use ($app, $db) {
 	}
 	else {
 		if($id == 'all') {
-			echo json_encode(get_all($db, $table));
+			echo json_encode(select_all($db, $table));
 		}
 		else if(is_numeric($id) && intval($id) > 0) {
 			$id = intval($id);
-			echo json_encode(get($db, $table, $id));
+			echo json_encode(select($db, $table, $id));
 		}
 		else {
 			$app->notFound();
@@ -38,7 +34,7 @@ $app->post('/:table', function($table) use ($app, $db) {
 	else {
 		___("post to $table");
 		$parameters = $app->request->post();
-		___($parameters);
+		insert($db, $table, $parameters);
 	}
 });
 $app->delete('/:table/:id', function($table, $id) use ($app, $db){
