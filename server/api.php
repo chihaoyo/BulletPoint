fe12a246aa8f4863c7d60c4368b543a003dc5e07
chihaoyo <chihaoyo@gmail.com>
2014-08-17 15:59:48 -0400

    Check table name, test $app->notFound()

--- server/api.php
+++ server/api.php
@@ -3,6 +3,10 @@
 include_once('importer.php');
 include_once('api-get.php');
 
+function table_exists($table) {
+	return in_array($table, array('Nodes', 'Edges', 'Tags', 'NodeTagPairs'));
+}
+
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
@@ -10,31 +14,37 @@ $db = connect_to_db();
 
 $app = new \Slim\Slim();
 $app->get('/:table/:id', function($table, $id) use ($app, $db) {
-	$table = ucfirst($table);
-	// Have to check $table for SQL injection here
-	
-	if($id == 'all') {
-		echo json_encode(get_all($db, $table));
-	}
-	else if(is_numeric($id) && intval($id) > 0) {
-		$id = intval($id);
-		echo json_encode(get($db, $table, $id));
+	// check $table for SQL injection here
+	if(!table_exists($table)) {
+		$app->notFound();
 	}
 	else {
+		if($id == 'all') {
+			echo json_encode(get_all($db, $table));
+		}
+		else if(is_numeric($id) && intval($id) > 0) {
+			$id = intval($id);
+			echo json_encode(get($db, $table, $id));
+		}
+		else {
+			$app->notFound();
+		}
+	}
+});
+$app->post('/:table', function($table) use ($app, $db) {
+	if(!table_exists($table)) {
 		$app->notFound();
 	}
 
-});
-$app->post('/:table', function($table) use ($app, $db) {
-	$table = ucfirst($table);
-	
 	___("post to $table");
 	$parameters = $app->request->post();
 	___($parameters);
 });
 $app->delete('/:table/:id', function($table, $id) use ($app, $db){
-	$table = ucfirst($table);
-	
+	if(!table_exists($table)) {
+		$app->notFound();
+	}
+
 	if(is_numeric($id) && intval($id) > 0) {
 		$id = intval($id);
 		___("delete $table #$id");
@@ -43,8 +53,9 @@ $app->delete('/:table/:id', function($table, $id) use ($app, $db){
 		$app->notFound();
 	}
 });
+
 $app->notFound(function() {
-	___('not available');
+	echo 'false';
 });
 $app->run();
 
