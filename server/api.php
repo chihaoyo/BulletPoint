1860e30d84130e56b1a200cd8f56d9e772fc670d
chihaoyo <chihaoyo@gmail.com>
2014-08-18 02:11:20 -0400

    Add Node, Tags, and NodeTagPairs

--- server/api.php
+++ server/api.php
@@ -41,7 +41,28 @@ $app->post('/:table', function($table) use ($app, $db) {
 	}
 	else {
 		$parameters = $app->request->post();
-		echo json_encode(write($db, $table, $parameters));
+		$r = write($db, $table, $parameters);
+
+		if($r !== false && $table == 'Nodes' && isset($parameters['comment']) && $parameters['comment'] != '') {
+			$node_id = $r;
+
+			// parse comment // add Tags
+			$words = explode(' ', $parameters['comment']);
+			$tag_ids = array();
+			foreach($words as $word) {
+				if($word{0} != '#')
+					continue;
+				$tag_ids.push(write($db, 'Tags', array('tag' => $word)));
+			}
+			if(count($tag_ids) > 0) {
+				// delete NodeTagPairs // add NodeTagPairs
+				delete_node_tag_pairs_on_node($db, $node_id);
+				foreach($tag_ids as $tag_id)
+					write($db, 'NodeTagPairs', array('node_id' => $node_id, 'tag_id' => $tag_id));
+			}
+		}
+
+		echo json_decode($r);
 	}
 });
 $app->delete('/:table/:id', function($table, $id) use ($app, $db){
@@ -51,11 +72,20 @@ $app->delete('/:table/:id', function($table, $id) use ($app, $db){
 	else {
 		if(is_numeric($id) && intval($id) > 0) {
 			$id = intval($id);
-			___("delete $table #$id");
+			echo json_decode(delete($db, $table, $id));
 		}
 		else {
 			$app->notFound();
 		}
+	}
+});
+$app->delete('/NodeTagPairs/Node/:node_id', function($table, $id) use ($app, $db){
+	if(is_numeric($node_id) && intval($node_id) > 0) {
+		$node_id = intval($node_id);
+		echo json_decode(delete_node_tag_pair_on_node($db, $node_id));
+	}
+	else {
+		$app->notFound();
 	}
 });
 $app->notFound(function() {
