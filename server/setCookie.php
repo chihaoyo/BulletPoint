fa97db442d09e9cbafc400027c7f8fba7371230e
chihaoyo <chihaoyo@gmail.com>
2014-08-19 00:13:07 -0400

    Fix guid

--- server/setCookie.php
+++ server/setCookie.php
@@ -1,19 +1,18 @@
 <?php
 
+// http://guid.us/
 function guid() {
 	mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
 	$charid = strtoupper(md5(uniqid(rand(), true)));
 	$hyphen = chr(45); // "-"
-	$uuid = chr(123) // "{"
-		.substr($charid, 0, 8).$hyphen
-		.substr($charid, 8, 4).$hyphen
-		.substr($charid,12, 4).$hyphen
-		.substr($charid,16, 4).$hyphen
-		.substr($charid,20,12)
-		.chr(125); // "}"
+	$uuid = substr($charid, 0, 8) . $hyphen
+		.substr($charid, 8, 4) . $hyphen
+		.substr($charid,12, 4) . $hyphen
+		.substr($charid,16, 4) . $hyphen
+		.substr($charid,20,12);
 	return $uuid;
 }
 
-setcookie('BulletPointUserID', guid(), time() + 3650*3600*1000); // 3650 days
+setcookie('BulletPointUserID', '@' . guid(), time() + 3650*3600*1000); // 3650 days
 
 ?>
