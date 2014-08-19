185d5e3873e0728b9bb0c645d83d948db7c1608c
chihaoyo <chihaoyo@gmail.com>
2014-08-19 19:12:48 +0000

    Update tag parser

--- server/api-parsing.php
+++ server/api-parsing.php
@@ -1,8 +1,8 @@
 <?php
 
 function find_tags($str) {
-	$special_chars = ',.()[]{}\'"“”‘’?!<>:;/\\';
-	$ending_chars = '\s\0\x0B' . preg_quote($special_chars, '/') . '，。：；？！「」『』〈〉《》';
+	$special_chars = ',.()[]{}\'"“”‘’?!<>:;/\\|';
+	$ending_chars = '\s\0\x0B' . preg_quote($special_chars, '/') . '，。、：；？！「」『』〈〉《》／｜＼';
 	$pattern = '/#[^' . $ending_chars . ']+/iu'; // i: case-insensitive, u: support for UTF-8
 	
 	preg_match_all($pattern, $str, $matches);
