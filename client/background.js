cd8b7f2263f1a2870cef14c09860fc64405e4e02
chihaoyo <chihaoyo@gmail.com>
2014-08-19 13:17:54 -0400

    Repurpose BrowserAction (button)

--- client/background.js
+++ client/background.js
@@ -1,8 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-/*	console.log('Add ' + tab.url + ' to BulletPoint.');	
-	chrome.tabs.executeScript({
-		code: 'document.body.style.backgroundColor="red"'
-	});*/
-	chrome.tabs.insertCSS({file: "script.css"});
-	chrome.tabs.executeScript({file: "script.js"});
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+    var url = 'http://50.18.115.212/bulletpoint/';
+    chrome.tabs.create({url: url});
 });
